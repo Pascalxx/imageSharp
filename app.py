0731acb1200d45869721bca271bb65854ce4e74e
Pascal <ro020701@gmail.com>
2022-11-30 20:50:14 +0900

    2022.11.30 add do_first_order method

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
 import numpy as np
-import cv2  # pip install -i https://pypi.douban.com/simple opencv-python==4.5.3.56
+import cv2
 
 
-def image_sharp():
-    img = cv2.imread('gData/lena_std.jpg')
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 轉灰階
-
+# Laplacian
+def do_laplacian(img_input):
     dA = np.array([-1, -1, -1, -1, 8, -1, -1, -1, -1])
     # dA = np.array([0, -1, 0, -1, 4, -1, 0, -1, 0])
-    out_img = np.empty([img.shape[0], img.shape[1], 3])
+    out_img = np.empty([img_input.shape[0], img_input.shape[1], 3])
 
-    # Laplacian處理
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            d = (dA[0] * img[i - 1][j - 1] if (i > 0 and j > 0) else 0) \
-                + (dA[1] * img[i - 1][j] if (i > 0) else 0) \
-                + (dA[2] * img[i - 1][j + 1] if (i > 0 and j < img.shape[1] - 1) else 0) \
-                + (dA[3] * img[i][j - 1] if (j > 0) else 0) \
-                + (dA[4] * img[i][j]) \
-                + (dA[5] * img[i][j + 1] if (j < img.shape[1] - 1) else 0) \
-                + (dA[6] * img[i + 1][j - 1] if (i < img.shape[0] - 1 and j > 0) else 0) \
-                + (dA[7] * img[i + 1][j] if (i < img.shape[0] - 1) else 0) \
-                + (dA[8] * img[i + 1][j + 1] if (i < img.shape[0] - 1 and j < img.shape[1] - 1) else 0)
+    # 逐一處理像素
+    for i in range(1, img_input.shape[0] - 1):
+        for j in range(1, img_input.shape[1] - 1):
+            d = (dA[0] * img_input[i - 1][j - 1] if (i > 0 and j > 0) else 0) \
+                + (dA[1] * img_input[i - 1][j] if (i > 0) else 0) \
+                + (dA[2] * img_input[i - 1][j + 1] if (i > 0 and j < img_input.shape[1] - 1) else 0) \
+                + (dA[3] * img_input[i][j - 1] if (j > 0) else 0) \
+                + (dA[4] * img_input[i][j]) \
+                + (dA[5] * img_input[i][j + 1] if (j < img_input.shape[1] - 1) else 0) \
+                + (dA[6] * img_input[i + 1][j - 1] if (i < img_input.shape[0] - 1 and j > 0) else 0) \
+                + (dA[7] * img_input[i + 1][j] if (i < img_input.shape[0] - 1) else 0) \
+                + (dA[8] * img_input[i + 1][j + 1] if (
+                    i < img_input.shape[0] - 1 and j < img_input.shape[1] - 1) else 0)
 
             # d[0] = 255 if d[0] > 255 else d[0]
             # d[0] = 0 if d[0] < 0 else d[0]
@@ -41,7 +40,39 @@ def image_sharp():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+    return out_img
+
+
+# 一階微分處理
+def do_first_order(img_input):
+    out_img = np.empty([img_input.shape[0], img_input.shape[1], 3])
+
+    # 逐一處理像素
+    for i in range(1, img_input.shape[0] - 1):
+        for j in range(1, img_input.shape[1] - 1):
+            d1 = img_input[i + 1][j - 1] + 2 * img_input[i + 1][j] + img_input[i + 1][j + 1]
+            d2 = img_input[i - 1][j - 1] + 2 * img_input[i - 1][j] + img_input[i - 1][j + 1]
+            d3 = img_input[i - 1][j + 1] + 2 * img_input[i][j + 1] + img_input[i + 1][j + 1]
+            d4 = img_input[i - 1][j - 1] + 2 * img_input[i][j - 1] + img_input[i + 1][j - 1]
+            d = abs(d1 - d2) + abs(d3 - d4)
+
+            d = d / 255
+            d = 255 if d > 255 else d
+            d = 0 if d < 0 else d
+            out_img[i][j] = d
+
+    cv2.imshow('frame', out_img)
+    while True:
+        cv2.imshow('frame', out_img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    return out_img
+
 
 # # # # # # # # # # #
 if __name__ == '__main__':
-    image_sharp()
+    img = cv2.imread('gData/lena_std.jpg')
+    img_bw = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 轉灰階
+    # laplacian_img = do_laplacian(img_bw)  # 進行Laplacian處理
+    first_order_img = do_first_order(img_bw)  # 進行一階微分處理
